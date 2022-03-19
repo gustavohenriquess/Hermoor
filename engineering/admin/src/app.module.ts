4e4fd3beb97d6a1f58c577a211a7267b8e2bfbaf
Gustavo Henrique <gustavo._henrique@hotmail.com>
2022-03-19 13:39:31 -0300

    refactor: restart project

--- engineering/admin/src/app.module.ts
+++ engineering/admin/src/app.module.ts
@@ -1,18 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EmailBoxModule } from './emailBox/emailBox.module';
-import { EmailBoxFolderModule } from './emailBoxFolder/emailBoxFolder.module';
-import { DatabaseModule } from './infra/database/database.module';
 import { ConfigModule } from '@nestjs/config/dist/config.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    EmailBoxModule,
-    EmailBoxFolderModule,
-    DatabaseModule,
-  ],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- engineering/admin/src/emailBoxFolder/emailBoxFolder.providers.ts
+++
@@ -1,8 +0,0 @@
-import { EmailBoxFolder } from './emailBoxFolder.entity';
-
-export const emailBoxFolderProviders = [
-  {
-    provide: 'EMAIL_BOX_FOLDER_REPOSITORY',
-    useValue: EmailBoxFolder,
-  },
-];
