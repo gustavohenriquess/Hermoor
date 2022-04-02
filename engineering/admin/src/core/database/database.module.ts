c8325156d1fae8870861d923b26ccc9d517409c0
Gustavo Henrique <gustavo._henrique@hotmail.com>
2022-04-02 00:38:19 -0300

    refactor: file name change

--- engineering/admin/src/core/database/database.module.ts
+++ engineering/admin/src/core/database/database.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { sequelizeProviders } from './sequilize.provider';
+import { sequelizeProviders } from './sequelize.provider';
 
 @Module({
   providers: [...sequelizeProviders],
