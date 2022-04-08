6b246538cbf06672dcf3ca2c903ee2d071e846c5
Gustavo Henrique <gustavo._henrique@hotmail.com>
2022-04-07 23:13:59 -0300

    chore: identation

--- engineering/admin/src/emailBoxFolder/emailBoxFolder.service.ts
+++ engineering/admin/src/emailBoxFolder/emailBoxFolder.service.ts
@@ -24,6 +24,7 @@ export class EmailBoxFolderService {
     const emailBoxFolderData = await this.emailBoxFolderRepository.save(
       emailBoxFolder,
     );
+
     return emailBoxFolderData;
   }
 
